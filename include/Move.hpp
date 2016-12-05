6e6518f6c8974f97524098b72accc4faa2cba72f
student <student@localhost.localdomain>
2016-12-05 16:12:36 +0100

    Added classes Move and Position.

---
+++ include/Move.hpp
@@ -0,0 +1,29 @@
+/*
+ * Move.hpp
+ *
+ *  Created on: 5 gru 2016
+ *      Author: student
+ */
+
+#ifndef INCLUDE_MOVE_HPP_
+#define INCLUDE_MOVE_HPP_
+
+#include "Position.hpp"
+
+class Move
+{
+public:
+	Move(Position start, Position stop)
+		: m_start(start), m_stop(stop)
+	{}
+
+	Position getStart() { return m_start; }
+	Position getStop() { return m_stop; }
+
+private:
+
+	Position m_start;
+	Position m_stop;
+};
+
+#endif /* INCLUDE_MOVE_HPP_ */
