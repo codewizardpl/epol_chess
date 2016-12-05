36a219661dee3e0310ea9e26c8d48f4852371a4d
pyro2000 <pyro2000@poczta.fm>
2016-12-05 18:51:23 +0100

    Simple CMakeLists

--- src/ConsoleDisplay.cpp
+++ src/ConsoleDisplay.cpp
@@ -4,11 +4,11 @@
 ConsloleDisplay::ConsloleDisplay(){
 
 }
-ConsloleDisplay::Display(Board board){
-    using::std;
+
+void ConsloleDisplay::Display(Board board) {
     for (int high = 0; high <8; ++high) {
         for (int width = 0; width < 8; ++width) {
-            cout <<"";
+            std::cout <<"";
         }
     }
 }
