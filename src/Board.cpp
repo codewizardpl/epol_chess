5de6fb57447407336dfac24900d0045ec8c0c713
student <student@localhost.localdomain>
2016-12-06 12:01:00 +0100

    board is created

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,9 +1,10 @@
+#include <iostream>
 #include "Board.hpp"
 
 using namespace std;
 
-Board::Board() : m_figure(0) {
-
+Board::Board() : m_figure(nullptr) {
+    cout << "Board is ready!" << endl;
 }
 
 Figure &Board::get(int x, int y) {
