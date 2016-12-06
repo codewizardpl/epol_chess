f90af9998dc5172b392db7a050bd1d4fca635c88
student <student@localhost.localdomain>
2016-12-06 14:03:00 +0100

    board is ready

--- include/Board.hpp
+++ include/Board.hpp
@@ -1,15 +1,25 @@
 #include "Figure.hpp"
+#include "Move.hpp"
+#include <vector>
+#include <memory>
+
+using namespace std;
 
 class Board
 {
   private:
-	Figure *m_figure;
+     const int size = 8*8;
+     vector<unique_ptr<Figure> > m_figures;
 
+     int calcIndex(int x, int y);
+     Figure* getAndRemove(int x, int y);
 
   public:
     Board();
 
-    Figure &get(int x, int y);
-    void set(int x, int y, Figure &fig);
+    Figure& get(int x, int y);
+    void set(int x, int y, Figure *fig);
+
+    void moveFigure(Move m);
 };
 
--- src/Board.cpp
+++ src/Board.cpp
@@ -3,15 +3,41 @@
 
 using namespace std;
 
-Board::Board() : m_figure(nullptr) {
+Board::Board() {
     cout << "Board is ready!" << endl;
+    m_figures.resize(size);
 }
 
-Figure &Board::get(int x, int y) {
-
-	return *m_figure;
+int Board::calcIndex(int x, int y) {
+    return x + 8*y;
 }
 
-void Board::set(int x, int y, Figure &fig) {
+Figure& Board::get(int x, int y) {
+    int index = calcIndex(x, y);
 
+    return *m_figures[index].get();
+}
+
+Figure* Board::getAndRemove(int x, int y) {
+    int index = calcIndex(x, y);
+
+    return m_figures[index].release();
+}
+
+void Board::set(int x, int y, Figure *fig) {
+    int index = calcIndex(x, y);
+
+    m_figures[index].reset(fig);
+}
+
+void Board::moveFigure(Move m) {
+    int startCol = m.getStart().getHorizontal(); // column
+    int startRow = m.getStart().getVertical(); // row
+
+    Figure* tmpFig = getAndRemove(startCol, startRow);
+
+    int stopCol = m.getStop().getHorizontal(); // column
+    int stopRow = m.getStop().getVertical(); // row
+
+    set(stopCol, stopRow, tmpFig);
 }
