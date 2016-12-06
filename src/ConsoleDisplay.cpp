36ab1a00d16ead4381590df6dfa13fe645c563f0
student <student@localhost.localdomain>
2016-12-06 15:05:11 +0100

    extra space fix
    Signed-off-by: student <student@localhost.localdomain>

--- src/ConsoleDisplay.cpp
+++ src/ConsoleDisplay.cpp
@@ -7,7 +7,7 @@ ConsoleDisplay::ConsoleDisplay()
 
 }
 void ConsoleDisplay::Display(Board &board){
-    std::cout<< "+abcdefgh+" << std::endl;
+    std::cout<< "+ a b c d e f g h +" << std::endl;
     for (int width = 0; width <8; ++width) {
         std::cout<< width +1<<" ";
         for (int height  = 0; height < 8; ++height) {
@@ -17,7 +17,7 @@ void ConsoleDisplay::Display(Board &board){
         std::cout<< width +1;
         std::cout << std::endl;
     }
-    std::cout<< "+abcdefgh+" << std::endl;
+    std::cout<< "+ a b c d e f g h +" << std::endl;
 
 }
 
