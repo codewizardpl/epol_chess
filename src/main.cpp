d83aa8ed08515e887facc63129a2511bb025e070
student <student@localhost.localdomain>
2016-12-07 16:25:03 +0100

    just fix

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,22 @@
 #include "ChessGame.hpp"
 #include "PuzzleGame.hpp"
 
+enum Games {
+    CHESS,
+    PUZZLE
+}
+
 int main() {
 
 
     Game *game;
-    int game_selected;
+    enum Games game_selected;
     cout << "Select game: Chess (1) or Puzzle (2)" << endl;
     cin >> game_selected;
     
     switch (game_selected){
-        case 1: { game = new ChessGame(); break; }
-        case 2: { game = new PuzzleGame(); break; }
+        case CHESS: { game = new ChessGame(); break; }
+        case PUZZLE: { game = new PuzzleGame(); break; }
         default: game = new ChessGame();
     }
     game->setupBoard();
