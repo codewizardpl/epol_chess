4109bee34f6002749e89266ecdf77beba7b8ddc0
student <student@localhost.localdomain>
2016-12-07 16:00:19 +0100

    Selection added

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include "Board.hpp"
+#include "Game.hpp"
+#include "PuzzleGame.hpp"
 #include "ChessGame.hpp"
 
 int main() {
 
-    ChessGame game;
-    game.setupBoard();
-    game.run();
+    Game *game;
+    int game_selected;
+    cout << "Select game: Chess (1) or Puzzle (2)" << endl;
+    cin >> game_selected;
+    
+    switch (game_selected){
+        case 1: { game = new ChessGame(); break; }
+        case 2: { game = new PuzzleGame(); break; }
+        default: game = new ChessGame();
+    }
+    game->setupBoard();
+    game->run();
+    
+    delete game;
     
     return 0;
 }
